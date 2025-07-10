a726b680dbb53cd8d350e66e86836d95dd18f5af
Kavia AI <production@kavia.ai>
2025-07-10 13:32:02 +0000

    CheckPoint - cg80a2869a

---
+++ .init/.linter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/home-sale-assistant-7ee31578/frontend_app
+npm run lint
+ESLINT_EXIT_CODE=$?
+npm run build
+BUILD_EXIT_CODE=$?
+if [ $ESLINT_EXIT_CODE -ne 0 ] || [ $BUILD_EXIT_CODE -ne 0 ]; then
+   exit 1
+fi
+
